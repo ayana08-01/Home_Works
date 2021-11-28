9ad4faaf85526740ba1e50dfd39a856d87534479
ayana08-01 <ll5020967@gmail.com>
2021-11-28 12:32:51 +0600

    hw

---
+++ dz4_ayana_08_01/filter.js
@@ -0,0 +1,7 @@
+let array = [11, 4, 7, 30, 50];
+
+let Newarray = array.filter(function (elem) {
+    return elem > 10;
+})
+
+console.log(Newarray);
---
+++ dz4_ayana_08_01/index.js
@@ -0,0 +1,8 @@
+let nums = [1, 5, 9, 20, 40];
+
+let result = nums.map(function (elem) {
+    return elem * 2;
+
+})
+
+console.log(result);
